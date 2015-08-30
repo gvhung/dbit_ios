67bbbe8c7149d5974c31e1de4eec19ee0de06310
MinJun Kweon <Minz0000000@gmail.com>
2015-08-30 15:48:55 +0900

    set snackbar property

---
+++ OPDBit/Utils/SnackBar/MZSnackBar.h
@@ -0,0 +1,37 @@
+//
+//  MZSnackBar.h
+//  OPDBit
+//
+//  Created by 1000732 on 2015. 8. 30..
+//  Copyright (c) 2015년 Minz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+static const CGFloat MZSnackBarHeight = 60.0f;
+
+typedef void (^MZSnackBarAnimationCompletion)(BOOL finished);
+
+typedef NS_ENUM(NSInteger, MZSnackBarType){
+    MZSnackBarTypeAlert = 0,    // default
+    MZSnackBarTypeProgress
+};
+
+@interface MZSnackBar : UIView
+
+@property (strong, nonatomic) UILabel *messageLabel;
+@property (strong, nonatomic) UIButton *cancelButton;
+@property (strong, nonatomic) UIActivityIndicatorView *indicatorView;
+
+@property (strong, nonatomic) NSString *message;
+@property (nonatomic) MZSnackBarType type;
+
+@property (nonatomic) CGFloat waitingDurationForDisappear;
+@property (nonatomic) CGFloat duration;
+
+- (instancetype)initWithFrame:(CGRect)frame;
+
+- (void)animateToAppear;
+- (void)animateToDisappearWithCompletion:(MZSnackBarAnimationCompletion)completion;
+
+@end
---
+++ OPDBit/Utils/SnackBar/MZSnackBar.m
@@ -0,0 +1,30 @@
+//
+//  MZSnackBar.m
+//  OPDBit
+//
+//  Created by 1000732 on 2015. 8. 30..
+//  Copyright (c) 2015년 Minz. All rights reserved.
+//
+
+#import "MZSnackBar.h"
+
+@implementation MZSnackBar
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
